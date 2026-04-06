82e13a58b69fe4f9695302cad6c7104e3189792e
safecorners <safecorners.mobile@gmail.com>
2026-04-07 00:27:36 +0900

    조도 센서 예제 코드 추가

---
+++ components/light/light.ino
@@ -0,0 +1,13 @@
+const int lightPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(lightPin, INPUT);
+}
+
+void loop() {
+  int lightValue = analogRead(lightPin);
+  Serial.println(lightValue);
+
+  delay(500);
+}
